f6c91b667617446cc948c6d38207550d9d78fec8
Mahalakshmi <MahalakshmiN0321@gmail.com>
2025-04-23 12:53:08 +0530

    fourth

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MahalakshmiN0321/puppy
 
 go 1.24.0
+
+require github.com/MahalakshmiN0321/dog v0.0.0-20250423071738-9726ae48a4c6
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
-
+import (
+	"github.com/MahalakshmiN0321/dog"
+)
 func Bark() string{
 	return "woof"
 }
 
 func Barks() string{
 	return "woof woof woof!"
+}
+
+func BigBark() string{
+	return dog.WhenGrowUp(Bark())
+
+}
+
+func BigBarks() string{
+	return dog.WhenGrowUp(Bark())
+	
 }
